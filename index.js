768097be2ba38373da2da414f5d48bcba6d4128d
Kyler Long <kylerlong24@gmail.com>
2025-05-03 00:17:14 -0400

    start writing pseudocode for js

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 //add an eventListener to generate passwords button for "click" to 
-//produce a string of characters (numbers, letters, and special characters)
+//produce a string of characters (numbers, letters, and special characters) between
+//8 and 16 characters based on length value of slider
+
+//create function to get the value of slider
+
+//
