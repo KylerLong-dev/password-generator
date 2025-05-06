45ba9bebf9d8bcdaa9f9d53e9a58c3b8aeb591ff
Kyler Long <kylerlong24@gmail.com>
2025-05-06 17:43:13 -0400

    Create logic for copying passwords via addition of eventListeners

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # password-generator
 Random Password Generator Project
 
+This project was created to assist with the learning of vanilla JS concepts such as DOM manipulation, functions, eventListeners, for loops, and arrays. 
+
+An array of characters was created that included the alphabet, numbers, and special characters so that this could be accessed for random character generation when clicking the "generate passwords" button. 
+
+A slider was used to create a userInput for the length of the desired password. This sliderInput.value was used in the for loop as the end point to ensure proper length of the password. 
+
+Each random index generated from Math.random was stored in a randomIndex variable and accessed via chars[randomIndex#]. Chars were added pwOneText, pwTwoText, etc via concatenation and rendered via texrContent within the getCharacters function. 
+
+This function was called when the pwBtn was clicked. 
+
+Navigator.clip.writeText() method utilized with an EventListener to copy the pw.textContent. 
--- index.js
+++ index.js
@@ -2,10 +2,17 @@
 let sliderInput = document.querySelector(".slider");
 let sliderOutput = document.querySelector(".slider__length");
 let pwBtn = document.querySelector(".generate-password");
+
 let pwOne = document.querySelector(".pw-1");
 let pwTwo = document.querySelector(".pw-2");
 let pwThree = document.querySelector(".pw-3");
 let pwFour = document.querySelector(".pw-4");
+
+let copyOne = document.querySelector(".copy-1");
+let copyTwo = document.querySelector(".copy-2");
+let copyThree = document.querySelector(".copy-3");
+let copyFour = document.querySelector(".copy-4");
+
 let chars = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
@@ -43,8 +50,31 @@ function getCharacters () {
     pwTwo.textContent = pwTwoText; 
     pwThree.textContent = pwThreeText; 
     pwFour.textContent = pwFourText; 
+    
 }
 
 pwBtn.addEventListener("click", () => {
     getCharacters();
 });
+
+copyOne.addEventListener("click", () => {
+    navigator.clipboard.writeText(pwOne.textContent);
+})
+
+copyTwo.addEventListener("click", () => {
+    navigator.clipboard.writeText(pwTwo.textContent);
+})
+
+copyThree.addEventListener("click", () => {
+    navigator.clipboard.writeText(pwThree.textContent);
+})
+
+copyFour.addEventListener("click", () => {
+    navigator.clipboard.writeText(pwFour.textContent);
+})
+
+
+
+
+
+
