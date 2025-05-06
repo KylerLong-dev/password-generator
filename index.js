90ad1779914e5ccf4eb833dda4b8c002483489ff
Kyler Long <kylerlong24@gmail.com>
2025-05-05 21:42:43 -0400

    Create logic for random password generator

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
-//add an eventListener to generate passwords button for "click" to 
-//produce a string of characters (numbers, letters, and special characters) between
-//8 and 16 characters based on length value of slider
 
-//create function to get the value of slider
+let sliderInput = document.querySelector(".slider");
+let sliderOutput = document.querySelector(".slider__length");
+let pwBtn = document.querySelector(".generate-password");
+let pwOne = document.querySelector(".pw-1");
+let pwTwo = document.querySelector(".pw-2");
+let pwThree = document.querySelector(".pw-3");
+let pwFour = document.querySelector(".pw-4");
+let chars = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+    '_', '+', '[', ']', '{', '}', '|', ';', ':', ',',
+    '.', '<', '>', '?', '/', '~', '`', '-', '=', '\\'
+  ];
 
-//
+sliderInput.addEventListener("input", () => {
+    sliderOutput.textContent = "Length: " + sliderInput.value; 
+})
+
+function getCharacters () {
+    let pwOneText = "";
+    let pwTwoText = "";
+    let pwThreeText = "";
+    let pwFourText = "";
+
+    for (let i = 0; i < sliderInput.value; i++) {
+        let randomIndexOne = Math.floor(Math.random() * chars.length);
+        let randomIndexTwo = Math.floor(Math.random() * chars.length);
+        let randomIndexThree = Math.floor(Math.random() * chars.length);
+        let randomIndexFour = Math.floor(Math.random() * chars.length);
+
+        pwOneText += chars[randomIndexOne];
+        pwTwoText += chars[randomIndexTwo];
+        pwThreeText += chars[randomIndexThree];
+        pwFourText += chars[randomIndexFour];
+    }
+
+    pwOne.textContent = pwOneText; 
+    pwTwo.textContent = pwTwoText; 
+    pwThree.textContent = pwThreeText; 
+    pwFour.textContent = pwFourText; 
+}
+
+pwBtn.addEventListener("click", () => {
+    getCharacters();
+});
